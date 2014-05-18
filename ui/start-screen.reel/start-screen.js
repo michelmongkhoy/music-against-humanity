4353af700956978dee8fd23cd6f15b005fe8ed84
Michel Mongkhoy <noreply>
2014-05-18 02:59:37 +0000

    Add component start-screen

---
+++ ui/start-screen.reel/start-screen.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/start-screen.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class StartScreen
+ * @extends Component
+ */
+exports.StartScreen = Component.specialize(/** @lends StartScreen# */ {
+    constructor: {
+        value: function StartScreen() {
+            this.super();
+        }
+    }
+});
