4eb1b3f1685c9393aa1ef7217790675f66d3f771
Michel Mongkhoy <noreply>
2014-05-18 03:01:21 +0000

    Add component start

---
+++ ui/start.reel/start.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/start.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Start
+ * @extends Component
+ */
+exports.Start = Component.specialize(/** @lends Start# */ {
+    constructor: {
+        value: function Start() {
+            this.super();
+        }
+    }
+});
